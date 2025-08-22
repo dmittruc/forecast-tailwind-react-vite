687192b2239ee5f5725a67277e3a5f0637a088b5
dmittruc <dimon22012201@gmail.com>
2025-08-22 16:50:33 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+/** @format */
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className="w-4">Hello world!</h1>
+      <h1 className="w-4">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
